cea0c8f70e41fa9e61229c1805492730864ada2a
Guillermo Gasca <luisgasca10@outlook.com>
2024-09-02 22:28:17 -0600

    Carousel Item add

--- src/components/ui/Carouseltem.tsx
+++ src/components/ui/Carouseltem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Carouseltem = () => {
   return (
-    <div className="bg-green-400 rounded ">
+    <div className="bg-green-400 rounded h">
       <img src="/plataforma.webp" className="rounded" alt="test" />
     </div>
   );
